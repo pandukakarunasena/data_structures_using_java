28f133649cb879a66507a707fa058a807a5fda19
panduka <kandyman777@gmail.com>
2021-02-10 12:35:25 +0530

    added  doubly linked list

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -10,30 +10,70 @@ class DoublyLinkedList {
     }
 
     private Node front;
-    private Node tail = front;
+    private Node tail;
 
     DoublyLinkedList(int data){
         front = new Node(data);
+        tail = front;
     }
 
     public void addToFront(int data){
         Node node = new Node(data);
         node.previous = front;
+        front.next = node;
         front = node;
     }
 
     public void addToTail(int data){
         Node node = new Node(data);
-
+        tail.previous = node;
+        node.next = tail;
+        tail = node;
     }
 
     public void removeFromFront(){
-
+        Node temp = front;
+        front = front.previous;
     }
 
     public void removeFromTail(){
-
+        Node temp = tail;
+        tail = tail.next;
     }
+
+    public void add(int data,int x ,String place){
+        if(place.equals("f")){
+            Node f = front;
+            while(f != null){
+                if(f.data == x){
+                    Node node = new Node(data);
+                    Node temp = f.next;
+
+                    f.next = node;
+                    node.next = temp;
+
+                    temp.previous = node;
+                    node.previous = f;
+                }
+                f = f.previous;
+            }
+        }
+        if(place.equals("b")){
+
+        }
+    };
+
+    public void printDLL(){
+        System.out.print("[");
+        Node temp = front;
+        while(temp != null){
+            System.out.print(temp.data +" ,");
+            temp = temp.previous;
+        }
+        System.out.println("]");
+    }
+
+
 }
 
 
